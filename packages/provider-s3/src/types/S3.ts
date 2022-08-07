106155876edafa8c6706c01ec6b2526aac6d0d9b
Adam Jarret <adam@atj.me>
2022-08-07 14:05:55 -0400

    Removed unused S3 types

--- packages/provider-s3/src/types/S3.ts
+++ packages/provider-s3/src/types/S3.ts
@@ -1,14 +1,10 @@
 import type {
   _Object,
   CopyObjectCommandInput,
-  CopyObjectCommandOutput,
   DeleteObjectCommandInput,
-  DeleteObjectCommandOutput,
   GetObjectCommandInput,
   ListObjectsV2CommandInput,
-  ListObjectsV2CommandOutput,
-  PutObjectCommandInput,
-  PutObjectCommandOutput
+  PutObjectCommandInput
 } from '@aws-sdk/client-s3';
 import { Configuration } from '@aws-sdk/lib-storage';
 
@@ -18,19 +14,9 @@ import { Configuration } from '@aws-sdk/lib-storage';
 export type S3CopyParams = CopyObjectCommandInput;
 
 /**
- * @see {@link https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/interfaces/copyobjectcommandoutput.html | CopyObjectCommandOutput}
- */
-export type S3CopyResult = CopyObjectCommandOutput;
-
-/**
  * @see {@link https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/interfaces/deleteobjectcommandinput.html | DeleteObjectCommandInput}
  */
 export type S3DeleteParams = DeleteObjectCommandInput;
-
-/**
- * @see {@link https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/interfaces/deleteobjectcommandoutput.html | DeleteObjectCommandOutput}
- */
-export type S3DeleteResult = DeleteObjectCommandOutput;
 
 /**
  * @see {@link https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/interfaces/getobjectcommandinput.html | GetObjectCommandInput}
@@ -43,20 +29,10 @@ export type S3GetParams = GetObjectCommandInput;
 export type S3ListParams = ListObjectsV2CommandInput;
 
 /**
- * @see {@link https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/interfaces/listobjectsv2commandoutput.html | ListObjectsV2CommandOutput}
- */
-export type S3ListResult = ListObjectsV2CommandOutput;
-
-/**
  * @see {@link https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/interfaces/putobjectcommandinput.html | PutObjectCommandInput}
  * @see {@link https://www.npmjs.com/package/@aws-sdk/lib-storage | Upload}
  */
 export type S3PutParams = PutObjectCommandInput & { multipart?: Partial<Configuration> };
-
-/**
- * @see {@link https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/interfaces/putobjectcommandoutput.html | PutObjectCommandOutput}
- */
-export type S3PutResult = PutObjectCommandOutput;
 
 /**
  * S3 file information returned by `listObjectsV2`
